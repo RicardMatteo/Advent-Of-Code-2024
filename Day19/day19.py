65b3d495cd227a254834ef216da14b62e0df1cda
Mattéo Ricard <ricardm@bde.enseeiht.fr>
2024-12-19 00:56:15 -0500

    feat: day 19 better

--- Day19/day19.py
+++ Day19/day19.py
@@ -3,13 +3,11 @@ from functools import lru_cache
 with open('Day19/input') as file:
     raw_input = file.read().split('\n')
 
-def count(towels, pattern):
-    @lru_cache
-    def rec(pattern):
-        if pattern == '': return 1
-        return sum(rec(pattern[len(towel):]) for towel in towels if pattern.startswith(towel))
-    return rec(pattern)
+@lru_cache
+def count(pattern):
+    if pattern == '': return 1
+    return sum(count(pattern[len(towel):]) for towel in towels if pattern.startswith(towel))
 
-towels,patterns = tuple(raw_input[0].split(', ')), raw_input[2:]
-solution = [count(towels, p) for p in patterns]
+towels,patterns = raw_input[0].split(', '), raw_input[2:]
+solution = [count(p) for p in patterns]
 print(sum(c > 0 for c in solution), sum(solution), sep='\n')
